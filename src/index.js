6b10ce62effe8ccea54196e20437879650afae14
mohammadbigdelu <si.bigdelu@gmail.com>
2023-09-14 18:05:08 -0700

    Build Commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "font-awesome/css/font-awesome.css";
 
 ReactDOM.render(
   <BrowserRouter>
