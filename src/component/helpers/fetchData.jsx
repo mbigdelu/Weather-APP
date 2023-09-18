e33319b33ce7360b6cb6e4b96eb47466c6202812
mohammadbigdelu <si.bigdelu@gmail.com>
2023-09-18 12:06:00 -0700

    Build Commit

--- src/component/helpers/fetchData.jsx
+++ src/component/helpers/fetchData.jsx
@@ -1,6 +1,6 @@
 const fetchData = async (location, type) => {
   try {
-    const URL = `http://api.weatherapi.com/v1/${type}.json?key=bee781d70f734c69bfa12520220902&q=${location}&aqi=no`;
+    const URL = `https://api.weatherapi.com/v1/${type}.json?key=bee781d70f734c69bfa12520220902&q=${location}&aqi=no`;
 
     const response = await fetch(URL);
 
