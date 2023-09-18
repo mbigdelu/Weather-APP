2b7114d1c0c79a2551e354250ea75c4e24adfe23
mohammadbigdelu <si.bigdelu@gmail.com>
2023-09-18 11:56:19 -0700

    Build Commit

--- src/component/helpers/fetchData.jsx
+++ src/component/helpers/fetchData.jsx
@@ -1,14 +1,18 @@
-import axios from "axios/dist/axios";
-
 const fetchData = async (location, type) => {
   try {
     const URL = `http://api.weatherapi.com/v1/${type}.json?key=bee781d70f734c69bfa12520220902&q=${location}&aqi=no`;
 
-    const response = await axios.get(URL);
+    const response = await fetch(URL);
 
-    return response.data;
-  } catch (e) {
-    console.log("there is no data for this name");
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error("An error occurred:", error.message);
   }
 };
 
